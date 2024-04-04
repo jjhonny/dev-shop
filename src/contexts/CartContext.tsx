134246d5ab35aa7cb6cf991104937ecfb35ca0c3
Jhonny Dias <ojhonny1@hotmail.com>
2024-04-04 15:08:03 -0400

    feat: completes cart logic

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -5,6 +5,8 @@ interface CartContextData {
   cart: CartProps[];
   cartAmount: number;
   addItemCart: (newItem: ProductsProps) => void;
+  removeItemCart: (product: CartProps) => void;
+  total: string;
 }
 
 interface CartProps {
@@ -25,6 +27,7 @@ export const CartContext = createContext({} as CartContextData);
 
 function CartProvider({ children }: CartProviderProps) {
   const [cart, setCart] = useState<CartProps[]>([]);
+  const [total, setTotal] = useState("");
 
   function addItemCart(newItem: ProductsProps) {
     const indexItem = cart.findIndex((item) => item.id === newItem.id);
@@ -37,6 +40,7 @@ function CartProvider({ children }: CartProviderProps) {
         cartList[indexItem].amount * cartList[indexItem].price;
 
       setCart(cartList);
+      totalResultCart(cartList);
       return;
     }
 
@@ -47,6 +51,39 @@ function CartProvider({ children }: CartProviderProps) {
     };
 
     setCart((products) => [...products, data]);
+    totalResultCart([...cart, data]);
+  }
+
+  function removeItemCart(product: CartProps) {
+    const indexItem = cart.findIndex((item) => item.id === product.id);
+
+    if (cart[indexItem]?.amount > 1) {
+      let cartList = cart;
+
+      cartList[indexItem].amount = cartList[indexItem].amount - 1;
+      cartList[indexItem].total =
+        cartList[indexItem].total - cartList[indexItem].price;
+
+      setCart(cartList);
+      totalResultCart(cartList);
+      return;
+    }
+
+    const removeItem = cart.filter((item) => item.id !== product.id);
+    setCart(removeItem);
+    totalResultCart(removeItem);
+  }
+
+  function totalResultCart(items: CartProps[]) {
+    let myCart = items;
+    let result = myCart.reduce((acc, obj) => {
+      return acc + obj.total;
+    }, 0);
+    const resultFormated = result.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+    setTotal(resultFormated);
   }
 
   return (
@@ -55,6 +92,8 @@ function CartProvider({ children }: CartProviderProps) {
         cart,
         cartAmount: cart.length,
         addItemCart,
+        removeItemCart,
+        total,
       }}
     >
       {children}
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -3,7 +3,7 @@ import { CartContext } from "../../contexts/CartContext";
 import { Link } from "react-router-dom";
 
 export function Cart() {
-  const { cart } = useContext(CartContext);
+  const { cart, total, addItemCart, removeItemCart } = useContext(CartContext);
 
   return (
     <div className="w-full max-w-7xl mx-auto">
@@ -35,11 +35,17 @@ export function Cart() {
             })}
           </strong>
           <div className="flex items-center justify-center gap-3">
-            <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            <button
+              className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center"
+              onClick={() => removeItemCart(item)}
+            >
               -
             </button>
             <span>{item.amount}</span>
-            <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            <button
+              className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center"
+              onClick={() => addItemCart(item)}
+            >
               +
             </button>
           </div>
@@ -52,7 +58,7 @@ export function Cart() {
           </strong>
         </section>
       ))}
-      {cart.length !== 0 && <p className="font-bold mt-4 ">Total: R$1.000</p>}
+      {cart.length !== 0 && <p className="font-bold mt-4 ">Total: {total}</p>}
     </div>
   );
 }
