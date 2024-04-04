a974e02987216958c27f5f95fe3587de0ff9b32d
Jhonny Dias <ojhonny1@hotmail.com>
2024-04-04 10:52:49 -0400

    feat: create page cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,26 @@
 export function Cart() {
   return (
-    <div>
-      <h1>Página Cart</h1>
+    <div className="w-full max-w-7xl mx-auto">
+      <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
+      <section className="flex items-center justify-between border-b-2 border-gray-300">
+        <img
+          className="w-28"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj7FMd0E3oNUXTxv84Cbh51SZl7PXJMUPD14UdNNIyiQ&s"
+          alt="Logo do produto"
+        />
+        <strong>Preço: R$1.000</strong>
+        <div className="flex items-center justify-center gap-3">
+          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            -
+          </button>
+          <span>2</span>
+          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+            +
+          </button>
+        </div>
+        <strong className="float-right">SubTotal: R$1.000</strong>
+      </section>
+      <p className="font-bold mt-4">Total: R$1.000</p>
     </div>
   );
 }
